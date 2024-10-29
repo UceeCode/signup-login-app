7cae34922c6a750dc74a2a539cc65e6520f64b48
Franklin <writefranklyn@gmail.com>
2024-10-29 23:53:12 +0100

    add mongoDB connection

--- app.js
+++ app.js
@@ -11,3 +11,8 @@ const app = new express(); //creates an instance of an express application for h
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view-engine', 'ejs');
+
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDb connected"))
+    .catch(err => console.error("MongoDb connection error", err));
