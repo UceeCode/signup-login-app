7d3bb3b249f050234dc743d149f6458e4e775cb9
Franklin <writefranklyn@gmail.com>
2024-10-29 23:49:48 +0100

    initailze express app and set up middleware

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const mongoose = require('mongoose'); //imports mongoose which is used to connec
 const bodyParser = require('body-parser'); //middleware used to parse incoming request bodies usually form data
 const bcrypt = require('bcryptjs'); //for hashing passwords before storing in database
 const User = require('./model/User'); //mongoose model representing user.
+
+const app = new express(); //creates an instance of an express application for handling http requests, routes and middleware
+
+//middleware setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view-engine', 'ejs');
